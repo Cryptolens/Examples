22cddf1f993c438aef882b1a42a6e703b3feba47
artemlos <artem@artemlos.net>
2015-12-10 18:24:43 +0100

    Add Key Activation example in C# and VB.NET

---
+++ SKM Examples/SKM Examples CSharp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SKGL;
+
+namespace SKM_Examples_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KeyActivation();
+            Console.ReadLine();
+        }
+
+        static void KeyActivation()
+        {
+
+            string machineCode = SKM.getMachineCode(SKM.getSHA1); // using SHA1 as the hashing algorithm
+            string licensekey = "MNIVR-MGQRL-QGUZK-BGJHQ";
+
+            var keyinfo = SKM.KeyActivation(pid: "3", uid: "2", hsum: "751963", 
+                                            sid: licensekey, mid:  machineCode);
+
+            var newKey = keyinfo.NewKey;
+
+            if (keyinfo.IsValid())
+            {
+                //valid key
+
+                Console.WriteLine("Created:" + keyinfo.CreationDate.ToShortDateString());
+                Console.WriteLine("Expired?: " + keyinfo.HasNotExpired().IsValid());
+
+            }
+            else
+            {
+                //invalid key
+                Console.WriteLine("Failed activation.");
+            }
+
+        }
+    }
+}
---
+++ SKM Examples/SKM Examples VBNET/Module1.vb
@@ -0,0 +1,32 @@
+﻿Imports SKGL
+Module Module1
+
+    Sub Main()
+        KeyActivation()
+
+    End Sub
+
+
+    Private Sub KeyActivation()
+
+        Dim machineCode As String = SKM.getMachineCode(AddressOf SKM.getSHA1)
+        ' using SHA1 as the hashing algorithm
+        Dim licensekey As String = "MNIVR-MGQRL-QGUZK-BGJHQ"
+
+        Dim keyinfo = SKM.KeyActivation(pid:="3", uid:="2", hsum:="751963", sid:=licensekey, mid:=machineCode)
+
+        Dim newKey = keyinfo.NewKey
+
+        If keyinfo.IsValid() Then
+            'valid key
+
+            Console.WriteLine("Created:" + keyinfo.CreationDate.ToShortDateString())
+
+            Console.WriteLine("Expired?: " + keyinfo.HasNotExpired().IsValid())
+        Else
+            'invalid key
+            Console.WriteLine("Failed activation.")
+        End If
+
+    End Sub
+End Module
