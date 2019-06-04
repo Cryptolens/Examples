02d9015a9bcbfb5f4725ac4c3a85307e06bfa205
Artem Los <arteml@kth.se>
2019-06-04 09:52:06 +0200

    Add example to check if payment is successful

---
+++ payments/check-if-payment-successful/Program.cs
@@ -0,0 +1,33 @@
+﻿using RestSharp;
+using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://app.cryptolens.io/Ext");
+
+            var request = new RestRequest("CheckPayment", Method.POST);
+            request.AddParameter("id", "3"); // adds to POST or URL querystring based on Method
+            request.AddParameter("reference", "ch_1EhXLc20eEPYPuUEx0j4EZQa");
+            request.AddParameter("orderId", "1908");
+
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+
+            if (content.Contains("error"))
+            {
+                Console.WriteLine("The order does not exist");
+            }
+            else
+            {
+                Console.WriteLine("The order exists");
+            }
+
+
+            Console.WriteLine(content);
+        }
+    }
+}
