51f42427c998485c53da1afb6b2080648a3e7969
Artem Los <arteml@kth.se>
2019-08-15 14:47:20 +0200

    Add example of how to retrieve global medata on app start

---
+++ app-settings-data-objects/retrieving-system-settings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using SKM.V3;
+using SKM.V3.Models;
+using SKM.V3.Methods;
+using System.Linq;
+
+namespace retrieving_system_settings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var systemSettings = Data.ListDataObjects("", new ListDataObjectsModel
+            {
+                ReferencerType = DataObjectType.Product,
+                ReferencerId = 3349,  // <- the product id
+            });
+            
+            if(!Helpers.IsSuccessful(systemSettings) || systemSettings.DataObjects == null)
+            {
+                Console.WriteLine("Could not retrieve the settings.");
+            }
+
+            var settings = systemSettings.DataObjects.ToDictionary(x=> x.Name, x => x);
+
+            if(settings.ContainsKey("DOTNET_RUNTIME"))
+                Console.WriteLine(settings["DOTNET_RUNTIME"].StringValue);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
